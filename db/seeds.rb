3863157da8392e42cca26836bcb7c1ef20520b9a
Harig16 <95hari.gopal@gmail.com>
2021-09-13 22:48:27 +0530

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times.each do |blog|
+  Blog.create!(title: "Blog-#{blog}", body: "testing the blogs")
+end
+
+puts "10 blogs created!"
+
+6.times.each do |skill|
+  Skill.create!(title: "skill-#{skill}", percentage_utilized: 30)
+end
+
+puts "6 Skills created!"
+
+9.times.each do |portfolio|
+  Porfolio.create!(title: "Porfolio-#{portfolio}",
+  sub_title: "sample text",
+body: "testing the portfolio",
+main_image: "https://via.placeholder.com/350X150",
+thumb_image: "https://via.placeholder.com/200X150")
+end
+
+puts "9 portfolio created!"
