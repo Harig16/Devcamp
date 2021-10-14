7bc20b191eb364e7a3340edcfb628f8ce285954c
Harig16 <95hari.gopal@gmail.com>
2021-10-14 07:47:01 +0530

    Updated master layout file along with the helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to "Login", new_user_session_path) + "<br>".html_safe + (link_to "Register", new_user_registration_path) 
+      (link_to "Login", new_user_session_path, class: style) + " ".html_safe + (link_to "Register", new_user_registration_path, class: style) 
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
@@ -16,6 +16,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Rendered.copyright "Hari", "All are reserved."
+    DevcampViewTool::Rendered.copyright "DevCamp", "All are reserved."
   end
 end
