f1f790f0872a394f000131ba2ff3d8516d3801e2
Harig16 <95hari.gopal@gmail.com>
2021-10-25 10:23:45 +0530

    Implement button for image uploaded and helper method for placeholder

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,16 @@
 module PortfoliosHelper
+
+  def image_generator(height:, width: )
+    "https://via.placeholder.com/#{height}X#{width}"
+  end
+
+  def portfolio_img img, type
+    if img
+      img
+    elsif img && type == "thumb"
+      image_generator(height: 350, width: 200)
+    else img && type == "main"
+      image_generator(height: 600, width: 400)
+    end
+  end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -15,9 +15,9 @@ class Portfolio < ApplicationRecord
                                   reject_if: lambda { |attrs| attrs[:name].empty? }
 
   # Concerns used only for data change. Created Placeholder file in concerns folder and included in Portfolio and skill model
-  include Placeholder
+  #include Placeholder
 
-  validates_presence_of :title, :sub_title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :sub_title, :body #, :main_image, :thumb_image
 
   # without scope method
   def self.angular
@@ -32,10 +32,10 @@ class Portfolio < ApplicationRecord
   scope :rails_portfolios_items, -> { where(sub_title: "Ruby on Rails") }
 
   # set default value in model level after object initialize
-  after_initialize :set_defaults
+  #after_initialize :set_defaults
 
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
-    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
-  end
+  #def set_defaults
+  #  self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+  #  self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
+  #end
 end
