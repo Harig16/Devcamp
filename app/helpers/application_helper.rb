6594137dad96b7fe526682d31febcb62a82567a0
Harig16 <95hari.gopal@gmail.com>
2021-10-26 19:43:50 +0530

    Implemented alert helper method for flash and error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,4 +54,15 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:notice] || flash[:error] || flash[:alert])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Devcamp portfolio", sticky: false)
+  end
 end
